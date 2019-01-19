1b053ec5a828428284d530a6f1ffac0a048347dc
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2019-01-19 16:21:52 -0300

    removing StatusBarColor component of RootContainer.

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-import { StatusBarColor } from '../Components/Common';
 import AppNavigation from '../Navigation/AppNavigation';
 
 // Styles
@@ -11,7 +10,6 @@ class RootContainer extends Component {
 	render() {
 		return (
 			<View style={styles.applicationView}>
-				<StatusBarColor backgroundColor="#fff" barStyle="light-content" />
 				<AppNavigation />
 			</View>
 		);
